06059bc014721ff1b6b4cbfb8040f792f91dd62a
rgaponov <rgaponov@omnicomm.ru>
2018-07-15 12:32:45 +0300

    add jsp page

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,5 +1,34 @@
 package controller;
 
-public class UserController {
+import dao.UserDao;
+import entity.User;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/hello")
+public class UserController extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private static String INSERT_OR_EDIT = "/user.jsp";
+    private static String LIST_USER = "/listUser.jsp";
+
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/user.jsp").forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        User user = new User();
+        user.setName(request.getParameter("name"));
+        user.setEmail(request.getParameter("email"));
+        
+        RequestDispatcher view = request.getRequestDispatcher(LIST_USER);
+
+        view.forward(request, response);
+    }
 }
