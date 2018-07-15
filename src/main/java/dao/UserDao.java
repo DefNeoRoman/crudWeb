122e74831bb9a422f8f064ddaedb812111ccb82f
rgaponov <rgaponov@omnicomm.ru>
2018-07-15 12:11:18 +0300

    Initial commit

---
+++ src/main/java/dao/UserDao.java
@@ -0,0 +1,111 @@
+package dao;
+
+import db.InitDB;
+import entity.User;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class UserDao {
+
+    public void addUser(User user) {
+
+        try(Connection connection = InitDB.getConnection();
+            PreparedStatement preparedStatement = connection
+                .prepareStatement(
+            "insert into users(age,name,email,createdDate) values (?, ?, ?, ? )")) {
+
+                executeUpdate(preparedStatement,user);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public void executeUpdate(PreparedStatement preparedStatement, User user){
+        try{
+
+        int i = 1;
+        preparedStatement.setInt(i++, user.getAge());
+        preparedStatement.setString(i++, user.getName());
+        preparedStatement.setString(i++, user.getEmail());
+        preparedStatement.setDate(i++, new Date(System.currentTimeMillis()));
+        preparedStatement.executeUpdate();
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+    }
+    public void deleteUser(int userId) {
+        try(Connection connection = InitDB.getConnection();
+            PreparedStatement preparedStatement = connection
+                    .prepareStatement("delete from users where userid=?")
+        ) {
+
+            preparedStatement.setInt(1, userId);
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateUser(User user) {
+        try(Connection connection = InitDB.getConnection();
+            PreparedStatement preparedStatement = connection
+                    .prepareStatement(
+    "update users set age=?,name=?,email=?,createdDate=? where userid=?");
+
+        ) {
+            executeUpdate(preparedStatement,user);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<User> getAllUsers() {
+        List<User> users = new ArrayList<User>();
+        try(Connection connection = InitDB.getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("select * from users")
+
+        ) {
+
+            while (rs.next()) {
+                User user = new User();
+                user.setId(rs.getLong("id"));
+                user.setAge(rs.getInt("age"));
+                user.setName((rs.getString("name")));
+                user.setEmail(rs.getString("email"));
+                user.setCreatedDate(rs.getTimestamp("createdDate"));
+                users.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return users;
+    }
+
+    public User getUserById(int userId) {
+        User user = new User();
+        try(Connection connection = InitDB.getConnection();
+            PreparedStatement preparedStatement = connection.
+           prepareStatement("select * from users where userid=?")
+        )
+        {
+         preparedStatement.setInt(1, userId);
+            ResultSet rs = preparedStatement.executeQuery();
+            if (rs.next()) {
+                user.setId(rs.getLong("id"));
+                user.setAge(rs.getInt("age"));
+                user.setName(rs.getString("name"));
+                user.setEmail(rs.getString("email"));
+                user.setCreatedDate(rs.getTimestamp("createdDate"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return user;
+    }
+}
---
+++ src/main/resources/init.sql
@@ -0,0 +1,14 @@
+DROP SCHEMA IF EXISTS  crudApplication;
+
+CREATE SCHEMA crudApplication;
+
+use crudApplication;
+
+CREATE TABLE users
+(
+    id BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    age INT NOT NULL,
+    name VARCHAR(90) NOT NULL,
+    email VARCHAR(90) NOT NULL,
+    createdDate TIMESTAMP NOT NULL
+);
