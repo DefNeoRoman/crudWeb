514025da3a0971f364a99d6b875ad1b5aa9f727f
rgaponov <rgaponov@omnicomm.ru>
2018-07-21 11:12:59 +0300

    добавил комментарии

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -4,6 +4,6 @@ public class Application {
     // Почему-то если нету этого файла то web.xml не может загрузиться
 
     public static void main(String[] args) {
-
+        // запуск через томкат
     }
 }
