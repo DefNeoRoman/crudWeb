f1fab7e56affdb44d845ee662e650ab2891ff2e0
rgaponov <rgaponov@omnicomm.ru>
2018-07-21 11:18:08 +0300

    рефакторинг

--- src/main/java/PopulateDB.java
+++ src/main/java/PopulateDB.java
@@ -10,9 +10,12 @@ public class PopulateDB {
     public static void main(String[] args) {
         Random random = new Random();
         UserDao dao = new UserDao();
-            for(int i=0; i<1000; i++){
-                dao.addUser(new User(UUID.randomUUID().toString(),UUID.randomUUID()+"@MAIL.COM",i+random.nextInt()));
-            }
+        for (int i = 0; i < 1000; i++) {
+            dao.addUser(
+                    new User(UUID.randomUUID().toString(),
+                            UUID.randomUUID() + "@MAIL.COM",
+                            i + random.nextInt()));
+        }
 
     }
 }
